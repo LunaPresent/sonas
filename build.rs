ea03f04f4f3ff49b6e56f8c21be12dddb8175ffe
Luna Present <code@lunapresent.net>
2025-07-25 10:57:15 +0200

    build: change vergen-gix to vergen-git2 because the latter actually works

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
-use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
+use vergen_git2::{BuildBuilder, CargoBuilder, Emitter, Git2Builder};
 
 fn main() -> Result<()> {
 	let build = BuildBuilder::all_build()?;
-	let gix = GixBuilder::all_git()?;
+	let git = Git2Builder::all_git()?;
 	let cargo = CargoBuilder::all_cargo()?;
 	Emitter::default()
 		.add_instructions(&build)?
-		.add_instructions(&gix)?
+		.add_instructions(&git)?
 		.add_instructions(&cargo)?
 		.emit()
 }
